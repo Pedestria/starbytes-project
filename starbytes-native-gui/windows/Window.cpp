3a14eb5d40b402dbe92604159c1adc9d01538271
Pedestria <42527102+Pedestria@users.noreply.github.com>
2020-12-04 20:05:27 -0500

    gui inir

---
+++ starbytes-native-gui/windows/Window.cpp
@@ -0,0 +1,14 @@
+#include "starbytes/Base/Base.h"
+
+#include <Windows.h>
+
+
+#define STARBYTES_GUI_NAMESPACE namespace Starbytes::GUI {
+
+STARBYTES_GUI_NAMESPACE
+
+void help () {
+    
+};
+
+NAMESPACE_END
